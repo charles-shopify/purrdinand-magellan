d9a93d76370173ad32f8ba8524d45fa575c9e4bc
charles-shopify <gamageyasindu9@gmail.com>
2024-01-01 21:29:04 +0530

    c

---
+++ assets/section-product.js
@@ -0,0 +1,48 @@
+let currentComponent = 1;
+
+allComponents = document.querySelectorAll('.component');
+
+function showCurrentComponent() {
+
+  allComponents.forEach(component => {
+    component.style.display = 'none';
+
+  });
+
+  document.querySelector(`#component-${currentComponent}`).style.display = 'block';
+
+} 
+
+function handleNextClick(event) {
+  if (currentComponent < 3) {
+    event.preventDefault();
+    currentComponent++;
+    showCurrentComponent();
+    
+
+  }
+
+}
+
+function handleBackClick(event) {
+  if (currentComponent > 1) {
+    event.preventDefault();
+    currentComponent--;
+    showCurrentComponent();
+   
+  }
+}
+
+
+document.getElementById('nextBtn1').addEventListener('click', handleNextClick);
+document.getElementById('nextBtn2').addEventListener('click', handleNextClick);
+
+
+
+
+document.getElementById('backBtn1').addEventListener('click', handleBackClick);
+document.getElementById('backBtn2').addEventListener('click', handleBackClick);
+
+
+
+showCurrentComponent();
